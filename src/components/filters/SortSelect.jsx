d6be4cafc66b49bf88bc7521f3dbbc91fcfd01bb
RocAfella044 <mandeep.rajbhandari.a22@icp.edu.np>
2026-06-17 12:59:02 +0545

    changes

--- src/components/filters/SortSelect.jsx
+++ src/components/filters/SortSelect.jsx
@@ -16,7 +16,7 @@ export function SortSelect() {
   return (
     <div>
       <label htmlFor="sort" className="eyebrow mb-2 block">
-        Sort by
+        Sort 
       </label>
       <select
         id="sort"
