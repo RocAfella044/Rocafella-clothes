8a071db037608aa488f67722e388c66f21c6dd78
RocAfella044 <mandeep.rajbhandari.a22@icp.edu.np>
2026-06-18 16:56:13 +0545

    changes again

--- src/pages/OrderDetailPage.jsx
+++ src/pages/OrderDetailPage.jsx
@@ -71,7 +71,7 @@ export function OrderDetailPage() {
           </div>
           <div className="border border-line p-6">
             <div className="flex justify-between font-display text-lg">
-              <span>Total price </span>
+              <span>Total</span>
               <span className="font-mono">NPR {order.total.toFixed(2)}</span>
             </div>
           </div>
